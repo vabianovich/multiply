b9c666ec9749868150cba90e98b4d22f7cfdc3eb
nor <fabianvanacoley@gmail.com>
2025-03-11 00:52:26 +0100

    initial commit

---
+++ multiply.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int multiply(int a, int b) {
+    return a * b;
+}
+
+void multiply_and_log(int a, int b) {
+    int result = multiply(a, b);
+    cout << "result:" << endl;
+    cout << result << endl;
+}
+
+int main() {
+    int a, b;
+    cout << "first number" << endl;
+    cin >> a;
+    cout << "second number" << endl;
+    cin >> b;
+    multiply_and_log(a, b);
+    return 0;
+}
